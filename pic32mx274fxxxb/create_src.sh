c73769a4258ef97222f43123ef9fd20d80c50364
Kiffie van Haash <kiffie@users.noreply.github.com>
2019-12-23 13:24:18 +0100

    Added PIC/EDC file for PIC32MX274

--- pic32mx274fxxxb/create_src.sh
+++ pic32mx274fxxxb/create_src.sh
@@ -1,8 +1,6 @@
 #!/bin/sh
 
-EDCPATH=/opt/microchip/mplabx/v5.25/packs/Microchip/PIC32MX_DFP/1.0.53/edc
-
-edc2svd $EDCPATH/PIC32MX274F256B.PIC PIC32MX274F256B.svd
+edc2svd PIC32MX274F256B.PIC PIC32MX274F256B.svd
 python3 svdpatch.py svdpatch.yaml
 svd2rust --target none -i PIC32MX274F256B.svd.patched
 rm -rf src
