5120fef2742197a423270228b8441bf98ea0d5d3
Kiffie van Haash <kiffie@users.noreply.github.com>
2020-01-13 22:52:49 +0100

    centralized patch spec; added symbolic ISR function names

--- pic32mx274fxxxb/create_src.sh
+++ pic32mx274fxxxb/create_src.sh
@@ -5,7 +5,9 @@ SVD=PIC32MX274F256B.svd
 
 edc2svd $EDC $SVD
 python3 ../svdpatch.py svdpatch.yaml
-svd2rust --target none -i "$SVD.patched"
+svd2rust --target none -i $SVD.patched
 rm -rf src
 form -i lib.rs -o src/ && rm lib.rs
 cargo fmt
+python3 ../svd2devicex.py -o device.x $SVD.patched
+
